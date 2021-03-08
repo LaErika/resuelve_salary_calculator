02415ae4617079529f773f9be462d849e35d7279
Erika Lopez <erika.1a.19@gmail.com>
2021-03-07 21:32:38 -0600

    Format

--- config/releases.exs
+++ config/releases.exs
@@ -8,4 +8,8 @@ config :salary_calculator_web, SalaryCalculatorWeb.Endpoint,
     host: System.get_env("SERVER", "localhost"),
     port: System.get_env("PORT", "4000") |> String.to_integer()
   ],
-  secret_key_base: System.get_env("SECRET_KEY_BASE", "hlfg/JeE3n9OtZ4qkQFe51XHFVkQmEBV/rfDWrxEU6cV4fkRYj0MtWSxcjK9ekz0")
+  secret_key_base:
+    System.get_env(
+      "SECRET_KEY_BASE",
+      "hlfg/JeE3n9OtZ4qkQFe51XHFVkQmEBV/rfDWrxEU6cV4fkRYj0MtWSxcjK9ekz0"
+    )
